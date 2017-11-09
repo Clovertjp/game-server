8621e59e030db70230965a6a6ae37ba459f24243
TangJP <395414469@qq.com>
2017-11-09 14:16:02 +0800

    add load xml

---
+++ game-common/src/main/java/com/game/common/server/config/GameConfigCache.java
@@ -0,0 +1,79 @@
+package com.game.common.server.config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author tangjp
+ *
+ */
+public class GameConfigCache {
+	
+	private static GameConfigCache gameConfigCache=new GameConfigCache();
+	
+	private Map<String,Map<String,String > > configItem;
+	private Map<String,Set<String > > configKeySet;
+	
+	
+	public static GameConfigCache getInstance(){
+		return gameConfigCache;
+	}
+	private GameConfigCache(){
+		configItem=new ConcurrentHashMap<>();
+		configKeySet=new HashMap<>();
+	}
+	
+	public void addItem(String fileName,String itemId,Map<String,String> val){
+		String key=fileName+"_"+itemId;
+		configItem.put(key, val);
+		if(!configKeySet.containsKey(fileName)){
+			configKeySet.put(fileName, new HashSet<>());
+		}
+		configKeySet.get(fileName).add(key);
+	}
+	
+	public Map<String,String> getItem(String fileName,String itemId){
+		return new HashMap<>(configItem.get(fileName+"_"+itemId));
+	}
+	
+	public List<Map<String,String > > getAllItem(String fileName){
+		List<Map<String,String > > list=new ArrayList<>();
+		if(!configKeySet.containsKey(fileName)){
+			return list;
+		}
+		for(String id : configKeySet.get(fileName)){
+			list.add(new HashMap<>(configItem.get(id)));
+		}
+		return list;
+	}
+	
+	public void clearItem(String fileName,String itemId){
+		String key=fileName+"_"+itemId;
+		configItem.remove(key);
+		if(!configKeySet.containsKey(fileName)){
+			return;
+		}
+		configKeySet.get(fileName).remove(key);
+	}
+	
+	public void clearAllItem(String fileName){
+		if(!configKeySet.containsKey(fileName)){
+			return ;
+		}
+		for(String id : configKeySet.get(fileName)){
+			configItem.remove(id);
+		}
+		configKeySet.get(fileName).clear();
+	}
+	
+	public void clearAllFile(){
+		configKeySet.clear();
+		configItem.clear();
+	}
+
+}
---
+++ game-common/src/main/java/com/game/common/server/config/GameConfigXml.java
@@ -0,0 +1,105 @@
+package com.game.common.server.config;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.game.common.exception.GameException;
+
+
+/**
+ * @author tangjp
+ *
+ */
+public class GameConfigXml {
+	
+	private static final Logger logger = LogManager.getLogger(GameConfigXml.class);
+	
+	private static final String PATH="Resource";
+	
+	public static void loadAllXML() throws GameException{
+		logger.info("loading xml start");
+		File xmlPath = new File(PATH);
+		if(!xmlPath.isDirectory()){
+			throw new GameException(PATH+" i snot a directory");
+		}
+		
+		File[] fileList=xmlPath.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File pathname) {
+				// TODO Auto-generated method stub
+				String fileName = pathname.getName();
+				if(!fileName.matches("^.+\\.xml$")) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		});
+		
+		GameConfigCache.getInstance().clearAllFile();
+		SAXReader reader = new SAXReader();
+		String fileName="";
+		try{
+			for(File file : fileList){
+				Document document = reader.read(file);
+				fileName=file.getName();
+				fileName=fileName.substring(0, fileName.length() - 4);
+				Element root=document.getRootElement();
+				loadXML(root,fileName);
+			}
+			
+			logger.info("loading xml finish");
+		}catch (Exception e) {
+			// TODO: handle exception
+			logger.error("loading xml is error,file name is "+fileName,e);
+			throw new GameException(fileName+" is error ",e);
+		}
+	}
+	
+	public static void readXMLByName(String fileName) throws GameException{
+		logger.info("loading "+fileName+" start");
+		File xml = new File(PATH+"/"+fileName+".xml");
+		GameConfigCache.getInstance().clearAllItem(fileName);
+		SAXReader reader = new SAXReader();
+		try{
+			Document document = reader.read(xml);
+			Element root=document.getRootElement();
+			loadXML(root,fileName);
+		}catch (Exception e) {
+			// TODO: handle exception
+			logger.error("loading xml is error,file name is "+fileName,e);
+			throw new GameException(fileName+" is error ",e);
+		}
+		
+	}
+	
+	private static void loadXML(Element root,String fileName) throws GameException{
+		Iterator<Element> iterator=root.elementIterator();
+		while(iterator.hasNext()){
+			Element element = iterator.next();
+			Map<String,String> attrMap=new HashMap<>();
+			List<Attribute> attrList = element.attributes();
+			for(Attribute attr : attrList){
+				attrMap.put(attr.getName(), attr.getValue());
+			}
+			if(!attrMap.containsKey("id")){
+				throw new GameException(fileName+" not contain id");
+			}
+			GameConfigCache.getInstance().addItem(fileName, attrMap.get("id"), attrMap);
+		}
+	}
+
+}
